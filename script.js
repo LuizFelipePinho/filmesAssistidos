7b7b5901f981a48695a03642cedca0e51445ceee
Luiz Felipe Pinho <47011421+LuizFelipePinho@users.noreply.github.com>
2021-09-23 12:36:19 -0300

    alterando a forma de mostrar os elementos na tela

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 
+const deleta = (id) => {
+    alert('deletando')
+}
 
 
 const botaoEnviar = document.querySelector('.enviar');
@@ -14,44 +17,78 @@ const salvaFilme = (nome, imagem, genero, nota) => {
         nome: nome,
         imagem: imagem,
         genero: genero,
-        nota: nota
+        nota: nota,
+        id: listaFilme.length + 1
     }
 
     listaFilme.push(obj)
-    console.log(listaFilme);
+    render(listaFilme)
+
 
 }
 
-const render = (nome, imagem, genero, nota) => {
+const render = (listaFilme) => {
+
     const container = document.querySelector('.container');
 
     const listItem = document.createElement('div')
 
-    // precisa fazer com q o genero e nome fiquem em cima da imagem 
-    listItem.innerHTML = `
+    // vai iterar por cada elemento da lista e armazenar o elemento dentro
+    const elemento = listaFilme.map( (elemento) => {
+        return `
+        <div class="box">
+                <div class="opcoesFilmes">
+                    <img class="imgEditar" src="botao-editar.png">
+                    <img class="imgDeletar" src="botao-de-deletar.png" onClick="deleta()">
     
-    <div class="box">
-            <div class="opcoesFilmes">
-                <img class="imgEditar" src="botao-editar.png">
-                <img class="imgDeletar" src="botao-de-deletar.png">
+                </div>
+                <p class="titulo-box">${elemento.nome}</p>
+                <img class="imagemBox" src="${elemento.imagem}" >
+                <div class="infos">
+                    <p class="infoGenero">${elemento.genero}</p>
+                    <p class="info">
+                    <img class="estrelaBox estrelaJS" src="estrela.png" >
+                    ${elemento.nota}/10
+                    </p>
+                </div>
+            </div
+        `
+    })
 
-            </div>
-            <p class="titulo-box">${nome}</p>
-            <img class="imagemBox" src="${imagem}" >
-            <div class="infos">
-                <p class="infoGenero">${genero}</p>
-                <p class="info">
-                <img class="estrelaBox" src="estrela.png" >
-                ${nota}/10
-                </p>
-    
-            </div>
-    
-        </div
-    
-    `
-
+    // simplesmente injetamos em listItem que é uma div e em seguida injetamos no nó do container
+    listItem.innerHTML = `${elemento}`
     container.appendChild(listItem)
+
+
+
+
+   
+
+    // precisa fazer com q o genero e nome fiquem em cima da imagem 
+    // listItem.innerHTML = `
+    
+    // <div class="box">
+    //         <div class="opcoesFilmes">
+    //             <img class="imgEditar" src="botao-editar.png">
+    //             <img class="imgDeletar" src="botao-de-deletar.png" onClick="deleta()">
+
+    //         </div>
+    //         <p class="titulo-box">${nome}</p>
+    //         <img class="imagemBox" src="${imagem}" >
+    //         <div class="infos">
+    //             <p class="infoGenero">${genero}</p>
+    //             <p class="info">
+    //             <img class="estrelaBox estrelaJS" src="estrela.png" >
+    //             ${nota}/10
+    //             </p>
+    
+    //         </div>
+    
+    //     </div
+    
+    // `
+
+    // container.appendChild(listItem)
 }
 
 
@@ -74,7 +111,6 @@ botaoEnviar.addEventListener("click", (evento) => {
     const nota = campoNota.value
 
     salvaFilme(nome, imagem, genero, nota)
-    render(nome, imagem, genero, nota)
 
     
 } )
