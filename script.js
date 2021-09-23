479181453e589dc85e4337bd967671e9635f54fe
Luiz Felipe Pinho <47011421+LuizFelipePinho@users.noreply.github.com>
2021-09-23 10:17:57 -0300

    layout do card

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 
+
+
+
 const botaoEnviar = document.querySelector('.enviar');
+
+
+
+
+const salvaFilme = (nome, imagem, genero, nota) => {
+    const listaFilme = []
+    
+    const obj = {
+        nome: nome,
+        imagem: imagem,
+        genero: genero,
+        nota: nota
+    }
+
+    listaFilme.push(obj)
+    console.log(listaFilme);
+
+}
+
+const render = (nome, imagem, genero, nota) => {
+    const container = document.querySelector('.container');
+
+    const listItem = document.createElement('div')
+
+    // precisa fazer com q o genero e nome fiquem em cima da imagem 
+    listItem.innerHTML = `
+    
+    <div class="box">
+            <div class="opcoesFilmes">
+                <img class="imgEditar" src="botao-editar.png">
+                <img class="imgDeletar" src="botao-de-deletar.png">
+
+            </div>
+            <p class="titulo-box">${nome}</p>
+            <img class="imagemBox" src="${imagem}" >
+            <div class="infos">
+                <p class="infoGenero">${genero}</p>
+                <p class="info">
+                <img class="estrelaBox" src="estrela.png" >
+                ${nota}/10
+                </p>
+    
+            </div>
+    
+        </div
+    
+    `
+
+    container.appendChild(listItem)
+}
+
+
+
+
 
 botaoEnviar.addEventListener("click", (evento) => {
     evento.preventDefault();
@@ -16,17 +73,10 @@ botaoEnviar.addEventListener("click", (evento) => {
     const campoNota = document.querySelector('#nota');
     const nota = campoNota.value
 
-    const container = document.querySelector('.container');
+    salvaFilme(nome, imagem, genero, nota)
+    render(nome, imagem, genero, nota)
 
-    const listItem = document.createElement('div')
-
-    // precisa fazer com q o genero e nome fiquem em cima da imagem 
-    listItem.innerHTML = `
-        <div class="box"> 
-            <div class="box-imagem"> <img src="${imagem}" > </div>
-            <p>${nome}</p>
-        </div
-    `
-
-    container.appendChild(listItem)
+    
 } )
+
+
